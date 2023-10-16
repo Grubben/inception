2a9584f2a57acf85d81d84d270c790857228d1c4
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r2s7.42lisboa.com>
2023-10-16 23:51:35 +0100

    ubuntu virtualbox work

--- srcs/requirements/wordpress/wpf.sh
+++ srcs/requirements/wordpress/wpf.sh
@@ -2,14 +2,29 @@
 # groupadd wordpress_user
 # useradd -g wordpress_user wordpress_user
 
-cd /var/www/
-curl https://wordpress.org/latest.tar.gz | tar zx
-mv wordpress/* .
-rm -d /var/www/wordpress
+# cd /var/www/
+# curl https://wordpress.org/latest.tar.gz | tar zx
+# mv wordpress/* .
+# rm -d /var/www/wordpress
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 # php wp-cli.phar --info
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-tail -f /dev/null
+cd /var/www/wordpress
+
+wp core download
+
+cp /wp-config.php /var/www/wordpress
+
+wp core install --allow-root --url=$URL --title=$TITLE --admin_user=root --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=root@42.fr
+wp user create --allow-root user user@user.user --user_pass=user
+
+chmod -R 0777 /var/www/wordpress/wp-content/
+
+echo Running php-fpm
+php-fpm81 -F -R
+
+echo php Failed
+# tail -f /dev/null
