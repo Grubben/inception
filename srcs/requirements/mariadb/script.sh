3545f83a1b3fd0b2c37f0f639bf1738a9c3b71fc
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r8s3.42lisboa.com>
2023-10-18 20:08:42 +0100

    Complete: removed ugly comments

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -7,23 +7,18 @@ fi
 
 if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 
-	# mysql_install_db --user=mysql --skip-test-db --basedir=/usr --datadir=/var/lib/mysql
 	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-	# mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-	# mysqld -u root --bootstrap <<EOF
 	mysqld -u mysql --bootstrap <<EOF
 		flush privileges;
 		create user '${MYSQL_USER}'@'%' identified by '${MYSQL_PASSWORD}';
 		CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 		grant all on ${MYSQL_DATABASE}.* to '${MYSQL_USER}'@'%';
 		delete from mysql.user where user='';
+		delete from mysql.user where user='root';
 		flush privileges;
 EOF
-		# delete from mysql.user where user='root';
 
 fi
 
 mysqld -u mysql
-# mysqld -u root
-# tail -f /dev/null
--- srcs/requirements/wordpress/wpf.sh
+++ srcs/requirements/wordpress/wpf.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
-# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# # php wp-cli.phar --info
-# chmod +x wp-cli.phar
-# mv wp-cli.phar /usr/local/bin/wp
+mkdir -p /var/www/wordpress
+cd /var/www/wordpress
 
 # wait for mariadb to start
 while ! mysql -h mariadb -P 3306 -u ${MYSQL_USER} -p${MYSQL_PASSWORD} 2> /dev/null; do
+echo waiting for mariadb...
 sleep 1
 done
 
-wp core is-installed 2> /dev/null
-if [ $? -ne 0 ] ; then
 	wp core download --allow-root
-	# wp config create --dbname=${MYSQL_DATABASE} \
     wp core config --dbname=${MYSQL_DATABASE} \
 				   --dbuser=${MYSQL_USER} \
 				   --dbpass=${MYSQL_PASSWORD} \
@@ -28,6 +24,5 @@ if [ $? -ne 0 ] ; then
 	wp user create ${WP_USER} \
 					${WP_USER_EMAIL} \
 					--user_pass=${WP_USER_PASS}
-fi
 
 php-fpm$PHP_V -FO
